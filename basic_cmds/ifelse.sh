11775324f711602f802b7a474862d32d5ff9b17f
Abinanthan <s.a.abinanthan2005@gmail.com>
2026-03-27 19:05:47 +0530

    notes.md updated

--- basic_cmds/ifelse.sh
+++ basic_cmds/ifelse.sh
@@ -9,3 +9,15 @@ else
 fi
 
 #-eq, -ne, -gt, -lt, -ge, -le for numeric comparisons
+
+score=75
+
+if [ $score -ge 90 ]; then
+  echo "Grade: A"
+elif [ $score -ge 75 ]; then
+  echo "Grade: B"
+elif [ $score -ge 60 ]; then
+  echo "Grade: C"
+else
+  echo "Grade: F"
+fi
