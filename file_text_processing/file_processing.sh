8501f64c170b534c4b9bfea653f83a443c4887a7
Abinanthan <s.a.abinanthan2005@gmail.com>
2026-03-28 23:53:11 +0530

    files modified

--- file_text_processing/file_processing.sh
+++ file_text_processing/file_processing.sh
@@ -19,7 +19,7 @@ echo "=== Highest salary first ==="
 sort -t',' -k3 -rn people.csv # -k3 Sorts using the 3rd column and -rn  ==  -r -n
 
 echo "=== Replace engineer with developer ==="
-sed 's/engineer/developer/g' people.csv
+sed 's/engineer/developer/g' people.csv # Replace ALL matches per line (global)
 
 
 # The << EOF block on line 3 is called a heredoc — a handy way to write multi-line content directly into a file from within a script.
